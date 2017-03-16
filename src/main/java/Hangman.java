c4f06abbcb3e3f89288fa27f3d1ba324dcd4aea4
chance magno and taylor loftis-kim <chance@>
2017-03-16 15:41:42 -0700

    Create three passing tests for Hangman app

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -3,13 +3,18 @@ import java.util.List;
 
   public class Hangman {
     private String mAnswer;
+    private String mUserInput;
 
-    public Hangman (String answer) {
+    public Hangman (String answer, String userInput) {
       mAnswer = answer;
+      mUserInput = userInput;
     }
 
     public String getAnswer() {
       return mAnswer;
     }
 
+    public String getUserInput() {
+      return mUserInput;
+    }
 }
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -5,15 +5,19 @@ public class HangmanTest {
 
   @Test
   public void newHangman_instantiatesCorrectly() {
-    Hangman testHangman = new Hangman("coding");
+    Hangman testHangman = new Hangman("coding", "c");
     assertEquals(true, testHangman instanceof Hangman);
   }
 
   @Test
   public void newHangman_getCorrectAnswer_true() {
-    Hangman testHangman = new Hangman("coding");
+    Hangman testHangman = new Hangman("coding", "c");
     assertEquals("coding", testHangman.getAnswer());
   }
 
-
+  @Test
+  public void newHangman_getLetterFromUser_true() {
+    Hangman testHangman = new Hangman("coding", "c");
+    assertEquals("c", testHangman.getUserInput());
+  }
 }
